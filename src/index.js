28d989d52dd3630248bd6fcc03d16a21c58eec36
Ritika Nair <Ritika_Nair@intuit.com>
2020-10-06 15:18:18 -0700

    test commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import FormattedMessage from './FormattedMessage';
 import TextProvider from './TextProvider';
 import withTextProvider from './TextProviderHOC';
-
+//test commit
 export {
   FormattedMessage,
   TextProvider,
